cd371942324e2267a42cbb7f47487c45a6d5a6fc
abratashev <abratashev@phoenixit.ru>
2018-05-23 00:28:34 +0300

    Initial commit

---
+++ resources/create_schema.sql
@@ -0,0 +1,19 @@
+-- — USER SQL
+CREATE USER cloud_service
+IDENTIFIED BY qwerty$4
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP
+ACCOUNT UNLOCK;
+
+-- privileges
+grant create procedure to cloud_service;
+grant create sequence to cloud_service;
+grant create session to cloud_service;
+grant create synonym to cloud_service;
+grant create table to cloud_service;
+
+-- — QUOTAS
+ALTER USER cloud_service QUOTA UNLIMITED ON USERS;
+
+-- — ROLES
+GRANT CONNECT TO cloud_service;
---
+++ src/main/Example.java
@@ -0,0 +1,30 @@
+package main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Example {
+
+    @Autowired
+    Scheduler scheduler;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            scheduler.reportCurrentTime();
+        };
+    }
+
+}
